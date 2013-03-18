195765ee1ed2a3d2e71f6963f1f2e6a5aba45b58
lindanXmu <lindan_xmu@126.com>
2013-03-19 01:48:58 +0800

    Basic CPU graph working hardcoded

--- src/main/java/edu/tsinghua/software/pages/monitor/Monitor.java
+++ src/main/java/edu/tsinghua/software/pages/monitor/Monitor.java
@@ -1,13 +1,18 @@
 package edu.tsinghua.software.pages.monitor;
 import org.apache.log4j.Logger;
-import org.apache.thrift.TException;
-
 import edu.tsinghua.software.cassandra.tools.DataManager;
 import edu.tsinghua.software.pages.template.BasePage;
 import edu.tsinghua.software.pages.template.MenuItemEnum;
 
 
-
+/**
+ * read graph.conf file, and send the attribute to javascript
+ * a list of : metric + tags
+ * JSON.stringify({'host':'ld-VirtualBox','type_instance':'idle','plugin_instance':'0'});
+ * host和time是在网页上选择，因此不用传输
+ * 需要传输的参数包括：
+ * {blockName:{picsName:    metric, a list of type_instance, a list of plugin_instance}}
+ * */
 public class Monitor extends BasePage {
     transient DataManager dataManager = new DataManager(getConnection());
      //log
